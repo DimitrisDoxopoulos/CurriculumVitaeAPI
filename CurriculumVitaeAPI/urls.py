467fd2b73fea58718c8573af25484944bb2faae8
DimitrisDoxopoulos <olonir@hotmail.com>
2024-08-15 13:28:53 +0300

    Changed urls.py

--- CurriculumVitaeAPI/urls.py
+++ CurriculumVitaeAPI/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken import views as drf_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/cv/', include('cv_api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', drf_views.obtain_auth_token),  # For token authentication
     path('api/blog/', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
